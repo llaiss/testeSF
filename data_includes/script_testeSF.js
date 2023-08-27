8bb8da0ff8bb1d6fd7aa5df1330347aa7c761388
llaiss <142757090+llaiss@users.noreply.github.com>
2023-08-27 17:09:06 -0300

    Update script_testeSF.js

--- data_includes/script_testeSF.js
+++ data_includes/script_testeSF.js
@@ -1,6 +1,8 @@
 PennController.ResetPrefix(null); // Comando essencial para o funcionamento dos demais comandos. Ele inativa os prefixos que acompanham as declarações de elemento do PennController.
+PennController.DebugOff();
 
-Sequence("Participante", "Instrucoes", randomize ("Experimento"), Sendresults(), "Historico", Sendresults(), "Final") // Define a ordem de aparecimento das telas.
+//Definindo a ordem de aparecimento das telas do experimento:
+Sequence("Participante", "Termo", "Instrucoes", randomize ("Experimento"), Sendresults(), "Hist", Sendresults(), "Final");
 //os comandos são os nomes das telas do experimento. obs- pcibex nao aceita caracteres especiais nem diacriticos
 //o Sequence serve também randomizar os itens de uma tela e enviar os resultados de uma das telas.
 // Sendresults - envia os resultados antecipadamente. 
@@ -25,15 +27,17 @@ Header(
             .css("font-size","1.2em")
             .center()
             .print()
-            .wait()       //importante mandar esperar! 
-
+            .wait()       //importante mandar esperar para que a pessoa possa clicar no botão.  
+				,
 )
 
 
-//Cria uma nova tela - Tela de coleta de dados do participante. Abaixo define-se como elas funcionarão.
+
+//Cria uma nova tela - Tela de coleta de dados do participante. Abaixo define-se como ela funcionará.
 newTrial("Participante",
 
-			newText("<p><b>TESTE DE AVALIAÇÃO DE SIMILARIDADE</b></p>") // aqui escreve o texto, colocando-o entre parágrafos. 
+//Texto inicial - aqui escreve o texto, colocando-o entre parágrafos. 
+			newText("<p><b>QUESTIONÁRIO DE AVALIAÇÃO DE SIMILARIDADE</b></p>") 
 			, 
 			newText("<p>Bem-Vind@s!</p>") 
 			, 
@@ -57,7 +61,7 @@ newTrial("Participante",
          ,
          newTextInput("Idade")
          ,
-         newText("<p>Agora selecione sua ESCOLARIDADE na caixa abaixo e aperte o botão 'Iniciar' para começar.</p>")
+         newText("<p>Agora selecione sua ESCOLARIDADE na caixa abaixo e aperte o botão 'Iniciar' para avançar.</p>")
          , 
 
 //Cria uma caixa com seletores nomeada "Escolaridade" para que o participante selecione sua escolaridade
@@ -87,16 +91,36 @@ newTrial("Participante",
          
 )
 
+//Envia para o arquivo "results" o conteúdo da variável "NOME"
 .log( "NOME" , getVar("NOME") )
 
 
 
+//Nova tela - Tela do termo de consentimento
+newTrial("Termo",
+         
+ 	newText("<p><b>TERMO DE CONSENTIMENTO LIVRE E ESCLARECIDO</b></p>")
+    ,
+	newText("Esta pesquisa est&aacute sendo desenvolvida pela aluna de mestrado do Programa de Pós-Graduação em Linguística da UFRJ Lais, sob a orienta&ccedil&atildeo do professor Marcus Maia.</p> A finalidade deste trabalho &eacute contribuir com a ci&ecircncia.</p> Solicitamos sua autoriza&ccedil&atildeo para utilizar seus dados em eventos da &aacuterea e publica&ccedil&otildees posteriores.</p> Basta voc&ecirc escrever EU CONCORDO na caixa de texto abaixo. "),
+newTextInput("tcle")
+.css("font-size","1.2em")
+    .print()
+    .log()
+,
+ newButton("Start")
+    .css("font-size","1.2em")
+        .print()
+        .wait()
+        )
 
+
+
+//Nova Tela - Instruções 
 newTrial("Instrucoes",
 
 			newText("<p><b>INSTRUÇÕES:</b></p>") 
 			, 
-			newText("<p>Avalie o grau de semelhança entre as pronúncias. Marque 1 para <u>nada similar</u> e 5 para <u>muito similar</u>, </p>") 
+			newText("<p>Avalie o grau de semelhança entre as pronúncias. Marque 1 para <u>nada similar</u> e 5 para <u>muito similar</u>.</p>") 
 			, 
 			newText("<p>Lembre-se: não há certo ou errado; o intuito do questionário é registrar a sua percepção.</p>") 
 			,
@@ -105,29 +129,5 @@ newTrial("Instrucoes",
          .log()
 
 
-//Indica o uso da tabela "treino_script_auditivo.csv"
-Template("tabela_script_auditivo.csv",
-// "variable" vai automaticamente apontar para cada linha da tabela "tabela_script_auditivo.csv"
-         row => newTrial( "Experimento",
-//"variable" aponta para todas as linhas da coluna "AudioExperimento" da tabela "tabela_script_auditivo.csv" e toca o audio referente a elas
-                  newAudio("AudioExperimento", row.AudioExperimento)
-                           .play()
-                  ,
-//Exibe na tela a imagem "alto_falante_icone.png"
-                  newImage("alto_falante_icone.png")
-                           .size( 90 , 90 )
-                           .print()
-       
-			            newScale("likert_scale", 5)
-											    .settings.labelsPosition("top")
-											    .settings.before( newText("muito_desacordo", "Muito Desacordo") )
-											    .settings.after( newText("muito_acordo", "Muito Acordo") )
-											    .print()
-											    .log()
-                          .wait();
 
-//Envia para o arquivo "results" o conteúdo da coluna "Group" 
-         .log("Group", row.Group)
-         .log("Item", row.item)
-);		
 
